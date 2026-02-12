ec7b265dadfee869e807c5628d5268ac255c2b95
Fuji <fujiji.entire1414@gmail.com>
2026-02-12 14:09:31 +0900

    change go version

--- flake.nix
+++ flake.nix
@@ -1,48 +1,40 @@
 {
   description = "A Go-based CLI to generate .dockerignore files, inspired by gibo.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      goVersion = "go1_25";
-    in {
-      packages.dibo = pkgs.buildGoModule {
-        pname = "dibo";
-        version = "0.1.0"; 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.dibo = pkgs.buildGoModule {
+          pname = "dibo";
+          version = "0.1.0";
 
-        src = ./.;
+          src = ./.;
 
-        vendorHash = null;
+          vendorHash = null;
 
-        proxyVendor = true;
+          go = pkgs.go_1_26;
 
-        buildFlags = [ "-mod=vendor" ];
+          ldflags = [ "-s" "-w" ];
 
-        ldflags = [ "-s" "-w" ];
-
-        go = pkgs.${goVersion};
-
-        meta = with pkgs.lib;
-          {
+          meta = with pkgs.lib; {
             description = "A Go-based CLI to generate .dockerignore files, inspired by gibo.";
             homepage = "https://github.com/anton-fuji/dibo";
             license = licenses.mit;
-            maintainers = [ "anton-fuji" ]; 
             platforms = platforms.linux ++ platforms.darwin;
           };
-      };
+        };
 
-      # nix-shell
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          gopls
-          go-tools
-          golangci-lint
-        ];
-      };
-    });
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_25
+            gopls
+            golangci-lint
+          ];
+        };
+      });
 }
